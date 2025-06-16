531bc8cd3354f09c8110c24470539630a7965220
Pedro Lins <pedrolins1309@gmail.com>
2025-06-15 23:34:35 -0300

    add tests for ppm writer

--- src/core/canvas.ml
+++ src/core/canvas.ml
@@ -3,23 +3,39 @@ module Canvas = struct
   let w t = t.w
   let h t = t.h
   let init w h = {w; h; pixels = (Array.make_matrix h w (Color.Color.init 0. 0. 0. 1.))}
+  let init_with_color w h c = {w; h; pixels = (Array.make_matrix h w c)}
   let write_pixel c x y color = 
-    c.pixels.(y).(x) <- color;
-    c
+    c.pixels.(y).(x) <- color;;
   let pixel_at x y c = c.pixels.(y).(x)
+  let scale_color c = 
+    if c < 0. then 0. else
+    if c > 1. then 255. else
+      c *. 255.
+
+  let color_to_spaced_rgb c = 
+    Printf.sprintf "%d %d %d" 
+      (int_of_float (scale_color (Color.Color.x c)))
+      (int_of_float (scale_color (Color.Color.y c)))
+      (int_of_float (scale_color (Color.Color.z c)))
   let canvas_to_ppm c =
-    let file = "out.ppm" in
-    let oc = open_out file in
-    Printf.fprintf oc "P3\n";
-    Printf.fprintf oc "%d %d\n" c.w c.h;
-    Printf.fprintf oc "255\n";
+    let b = Buffer.create 1024 in
+    Printf.bprintf b "P3\n";
+    Printf.bprintf b "%d %d\n" c.w c.h;
+    Printf.bprintf b "255\n";
     for y = 0 to c.h - 1 do
-      for x  = 0 to c.w - 1 do
-        Printf.fprintf oc "%d %d %d " 
-        (int_of_float (Color.Color.x c.pixels.(y).(x)))
-        (int_of_float (Color.Color.y c.pixels.(y).(x)))
-        (int_of_float (Color.Color.z c.pixels.(y).(x)));
-      done;
-      Printf.fprintf oc "\n"
+      let b_row = Buffer.create 1024 in
+        for x  = 0 to c.w - 1 do
+          let next_color = (color_to_spaced_rgb c.pixels.(y).(x)) in
+            if (String.length next_color) + (Buffer.length b_row) >= 70 then (
+              Buffer.add_char b_row '\n';
+              Buffer.add_buffer b b_row;
+              Buffer.clear b_row
+            );
+            Buffer.add_string b_row next_color;
+            if x = c.w - 1 then Buffer.add_char b_row '\n' else Buffer.add_char b_row ' ' 
+        done;
+        Buffer.add_buffer b b_row
     done;
-  end
+    Buffer.add_char b '\n';
+    b
+end
--- test/unit/chapter2.ml
+++ test/unit/chapter2.ml
@@ -34,11 +34,11 @@ let (-:) name f = Alcotest.test_case name `Quick f
         (Color.z result =. 0.8) && 
         (Color.w result =. 2.))
     end;
-    "blend colors" -: begin fun () ->
+    "hadamard product" -: begin fun () ->
       let c1 = Color.init 1. 0.2 0.4 1.0 in
       let c2 = Color.init 0.9 1. 0.1 1.0 in
       let result = ColorOps.(<*>) c1 c2 in 
-      Alcotest.(check bool) "subtracts color" true 
+      Alcotest.(check bool) "hadamard product" true 
         ((Color.x result =. 0.9) && 
         (Color.y result =. 0.2) && 
         (Color.z result =. 0.04) && 
@@ -46,20 +46,76 @@ let (-:) name f = Alcotest.test_case name `Quick f
     end;
     "create canvas" -: begin fun () ->
       let canvas = Canvas.init 10 20 in
-      Alcotest.(check bool) "subtracts color" true 
+      Alcotest.(check bool) "canvas has proper dimensions and all pixels are black initially" true 
         ((Canvas.w canvas = 10) && 
-        (Canvas.h canvas = 20))
+        (Canvas.h canvas = 20) &&
+        (Array.for_all 
+          (fun row -> (
+            Array.for_all 
+              (fun col -> (
+                (Color.x col = 0.) &&
+                (Color.y col = 0.) &&
+                (Color.z col = 0.) &&
+                (Color.w col = 1.)))
+              row)) 
+          canvas.pixels))
     end;
     "write pixels to canvas" -: begin fun () ->
       let canvas = Canvas.init 10 20 in
       let red = Color.init 1. 0. 0. 1. in
-      Alcotest.(check bool) "subtracts color" true 
-        ((Canvas.write_pixel canvas 5 5 red) |> Canvas.pixel_at 5 5 |> ColorOps.(=) red)
+      Alcotest.(check bool) "red pixel at (5, 5)" true 
+        ((Canvas.write_pixel canvas 5 5 red); 
+        Canvas.pixel_at 5 5 canvas |> ColorOps.(=) red)
     end;
     "write canvas to ppm" -: begin fun () ->
-      let canvas = Canvas.init 10 20 in
-      let red = Color.init 0. 255. 0. 1. in
-      Canvas.canvas_to_ppm (Canvas.write_pixel canvas 5 5 red);
-      Alcotest.(check bool) "subtracts color" true true
+      let canvas = Canvas.init 5 3 in
+      let ppm = Canvas.canvas_to_ppm (canvas) in
+      Alcotest.(check bool) "outputs expected ppm header" true
+      (String.starts_with (Buffer.contents ppm)  ~prefix:"P3\n5 3\n255")
     end;
-  ]
+    "write pixels to canvas and scales to ppm correct values" -: begin fun () ->
+      let canvas = Canvas.init 5 3 in
+      let c1 = Color.init 1.5 0. 0. 1. in
+      let c2 = Color.init 0. 0.5 0. 1. in
+      let c3 = Color.init (-0.5) 0. 1. 1. in
+      Canvas.write_pixel canvas 0 0 c1;
+      Canvas.write_pixel canvas 2 1 c2;
+      Canvas.write_pixel canvas 4 2 c3;
+      let ppm = Canvas.canvas_to_ppm canvas in
+      let b = Buffer.contents ppm in
+      let lines = (String.split_on_char '\n' b) in
+      Alcotest.(check bool) "outputs expected ppm file" true
+      (
+        match lines with
+        | _ :: _ :: _ :: line3 :: line4 :: line5 :: _ -> (
+            line3 = "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0" &&
+            line4 = "0 0 0 0 0 0 0 127 0 0 0 0 0 0 0" &&
+            line5 = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"
+          )
+        | _ -> false
+      )
+    end;
+    "write pixels to ppm and breaks lines  to be below 70 chars" -: begin fun () ->
+      let canvas = Canvas.init_with_color 10 2 (Color.init 1. 1. 1. 1.) in
+      let ppm = Canvas.canvas_to_ppm canvas in
+      let b = Buffer.contents ppm in
+      let lines = (String.split_on_char '\n' b) in
+      Alcotest.(check bool) "outputs expected ppm file" true
+      (
+        List.for_all (fun line-> (String.length line <= 70)) lines
+      )
+    end;
+    "ppm files are terminated by a newline" -: begin fun () ->
+      let canvas = Canvas.init_with_color 10 2 (Color.init 1. 1. 1. 1.) in
+      let ppm = Canvas.canvas_to_ppm canvas in
+      let b = Buffer.contents ppm in
+      let lines = (String.split_on_char '\n' b) in
+      (* Printf.printf "%s" b; *)
+      Alcotest.(check bool) "ends in newline" true
+      (
+        match List.rev lines with
+        | l :: _ -> (l = "")
+        | _ -> false
+      )
+    end;
+    ]
