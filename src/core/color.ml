f33d86ffbd0efffe4c90a2622c94302d790394f2
Pedro Lins <pedrolins1309@gmail.com>
2026-04-02 15:26:28 -0300

    chore: simplify Tuple and Color

--- src/core/color.ml
+++ src/core/color.ml
@@ -1,26 +1,3 @@
-type t = Matrix.t
-
-let x t = Matrix.get t 0 0
-let y t = Matrix.get t 1 0
-let z t = Matrix.get t 2 0
-let w t = Matrix.get t 3 0
-
-let init red green blue alpha =
-  let mat = Matrix.create 4 1 in
-  Matrix.set mat 0 0 red;
-  Matrix.set mat 1 0 green;
-  Matrix.set mat 2 0 blue;
-  Matrix.set mat 3 0 alpha;
-  mat
-
-include Tuple.Vec4Ops (struct
-  type t = Matrix.t
-
-  let x = x
-  let y = y
-  let z = z
-  let w = w
-  let init = init
-end)
+include Tuple
 
 let ( <*> ) a b = init (x a *. x b) (y a *. y b) (z a *. z b) (w a *. w b)
--- src/core/tuple.ml
+++ src/core/tuple.ml
@@ -1,60 +1,5 @@
 open Common
 
-module type Vec4 = sig
-  type t = Matrix.t
-
-  val x : t -> float
-  val y : t -> float
-  val z : t -> float
-  val w : t -> float
-  val init : float -> float -> float -> float -> t
-end
-
-module Vec4Ops (M : Vec4) = struct
-  let unite_components f a b =
-    M.init
-      (f (M.x a) (M.x b))
-      (f (M.y a) (M.y b))
-      (f (M.z a) (M.z b))
-      (f (M.w a) (M.w b))
-
-  let apply_to_components f a scalar =
-    M.init
-      (f (M.x a) scalar)
-      (f (M.y a) scalar)
-      (f (M.z a) scalar)
-      (f (M.w a) scalar)
-
-  let origin = M.init 0. 0. 0. 0.
-  let ( <+> ) a b = unite_components ( +. ) a b
-  let ( <-> ) a b = unite_components ( -. ) a b
-  let ( ~<-> ) a = unite_components ( -. ) origin a
-  let ( <*.> ) a scalar = apply_to_components ( *. ) a scalar
-  let ( </.> ) a scalar = apply_to_components ( /. ) a scalar
-  let is_point v = M.w v =. 1.0
-  let is_vector v = M.w v =. 0.
-  let magnitude a = sqrt ((M.x a ** 2.) +. (M.y a ** 2.) +. (M.z a ** 2.))
-
-  let normalize a =
-    let m = magnitude a in
-    M.init (M.x a /. m) (M.y a /. m) (M.z a /. m) (M.w a /. m)
-
-  let add_components a = M.x a +. M.y a +. M.z a +. M.w a
-  let dot a b = unite_components ( *. ) a b |> add_components
-
-  let cross a b =
-    let x = (M.y a *. M.z b) -. (M.z a *. M.y b) in
-    let y = (M.z a *. M.x b) -. (M.x a *. M.z b) in
-    let z = (M.x a *. M.y b) -. (M.y a *. M.x b) in
-    M.init x y z 0.
-
-  let init_point x y z = M.init x y z 1.
-  let init_vector x y z = M.init x y z 0.
-
-  let ( = ) a b =
-    M.x a =. M.x b && M.y a =. M.y b && M.z a =. M.z b && M.w a =. M.w b
-end
-
 type t = Matrix.t
 
 let x t = Matrix.get t 0 0
@@ -70,12 +15,35 @@ let init x y z w =
   Matrix.set mat 3 0 w;
   mat
 
-include Vec4Ops (struct
-  type t = Matrix.t
+let unite_components f a b =
+  init (f (x a) (x b)) (f (y a) (y b)) (f (z a) (z b)) (f (w a) (w b))
 
-  let x = x
-  let y = y
-  let z = z
-  let w = w
-  let init = init
-end)
+let apply_to_components f a scalar =
+  init (f (x a) scalar) (f (y a) scalar) (f (z a) scalar) (f (w a) scalar)
+
+let origin = init 0. 0. 0. 0.
+let ( <+> ) a b = unite_components ( +. ) a b
+let ( <-> ) a b = unite_components ( -. ) a b
+let ( ~<-> ) a = unite_components ( -. ) origin a
+let ( <*.> ) a scalar = apply_to_components ( *. ) a scalar
+let ( </.> ) a scalar = apply_to_components ( /. ) a scalar
+let is_point v = w v =. 1.0
+let is_vector v = w v =. 0.
+let magnitude a = sqrt ((x a ** 2.) +. (y a ** 2.) +. (z a ** 2.))
+
+let normalize a =
+  let m = magnitude a in
+  init (x a /. m) (y a /. m) (z a /. m) (w a /. m)
+
+let add_components a = x a +. y a +. z a +. w a
+let dot a b = unite_components ( *. ) a b |> add_components
+
+let cross a b =
+  let cross_x = (y a *. z b) -. (z a *. y b) in
+  let cross_y = (z a *. x b) -. (x a *. z b) in
+  let cross_z = (x a *. y b) -. (y a *. x b) in
+  init cross_x cross_y cross_z 0.
+
+let init_point x y z = init x y z 1.
+let init_vector x y z = init x y z 0.
+let ( = ) a b = x a =. x b && y a =. y b && z a =. z b && w a =. w b
