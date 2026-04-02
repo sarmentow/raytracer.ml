9b28476587597db018fc2889e0b0a38cf801765a
Pedro Lins <pedrolins1309@gmail.com>
2026-04-02 15:29:31 -0300

    chore: simplify build wrapping

--- src/raytracer.ml
+++
@@ -1,6 +0,0 @@
-module Tuple = Raytracer__core.Tuple
-module Color = Raytracer__core.Color
-module Canvas = Raytracer__core.Canvas
-module Matrix = Raytracer__core.Matrix
-module Common = Raytracer__core.Common
-module Transformations = Raytracer__core.Transformations
